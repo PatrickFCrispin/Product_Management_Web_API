157fe3ddfae58d5f3d18f807ba7a526805e4b870
patrickfonseca208 <patrickfonseca208@gmail.com>
2023-05-02 13:43:21 -0300

    Update application

---
+++ ProductManagement/ProductManagement.API/Program.cs
@@ -0,0 +1,50 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.API.DTOs;
+using ProductManagement.API.Services;
+using ProductManagement.API.ViewModels;
+using ProductManagement.Domain.Entities;
+using ProductManagement.Domain.Interfaces;
+using ProductManagement.Infra.Context;
+using ProductManagement.Infra.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkSqlServer()
+    .AddDbContext<DBContext>(x => x.UseSqlServer(
+        builder.Configuration.GetConnectionString("DataBase"), y => y.MigrationsAssembly("ProductManagement.API")));
+
+var mapperConfiguration = new MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<ProductViewModel, ProductDTO>();
+    cfg.CreateMap<ProductDTO, ProductEntity>();
+});
+IMapper mapper = mapperConfiguration.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
