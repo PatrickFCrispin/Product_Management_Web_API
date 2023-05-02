09d5bf2bb818d957c447cd068332c43d295ba74e
patrickfonseca208 <patrickfonseca208@gmail.com>
2023-05-02 02:01:18 -0300

    Add null condition to model

--- ProductManagement/ProductManagement/DTOs/ProductDTO.cs
+++ ProductManagement/ProductManagement/DTOs/ProductDTO.cs
@@ -3,9 +3,9 @@
     public class ProductDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public decimal Cost { get; set; }
-        public string Supplier { get; set; }
+        public string Supplier { get; set; } = null!;
         public bool Active { get; set; }
         public DateTime RegisteredAt { get; set; }
         public DateTime ModifiedAt { get; set; }
--- ProductManagement/ProductManagement/ViewModels/ProductViewModel.cs
+++ ProductManagement/ProductManagement/ViewModels/ProductViewModel.cs
@@ -5,9 +5,9 @@ namespace ProductManagement.API.ViewModels
     public class ProductViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public decimal Cost { get; set; }
-        public string Supplier { get; set; }
+        public string Supplier { get; set; } = null!;
         public bool Active { get; set; }
         public DateTime RegisteredAt { get; set; }
         public DateTime ModifiedAt { get; set; }
