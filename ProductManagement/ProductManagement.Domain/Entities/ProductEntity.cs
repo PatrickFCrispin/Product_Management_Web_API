0c823d244e780ef6257a601ec42d5c196beea80d
patrickfonseca208 <patrickfonseca208@gmail.com>
2023-06-12 10:22:17 -0300

    Update repository

--- ProductManagement/ProductManagement.Domain/Entities/ProductEntity.cs
+++ ProductManagement/ProductManagement.Domain/Entities/ProductEntity.cs
@@ -3,9 +3,9 @@
     public class ProductEntity
     {
         public int Id { get; set; }
-        public string Name { get; set; } = null!;
+        public string Name { get; set; } = default!;
         public decimal Price { get; set; }
-        public string Supplier { get; set; } = null!;
+        public string Supplier { get; set; } = default!;
         public bool Active { get; set; }
         public DateTime RegisteredAt { get; set; }
         public DateTime ModifiedAt { get; set; }
